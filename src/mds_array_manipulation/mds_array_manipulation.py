1d2d72f9197fe12adbf6c66c9e0423704b08fa48
Sean McKay <sean.mckay.314@gmail.com>
2024-01-11 19:59:25 -0800

    Finished skeleton

---
+++ src/mds_array_manipulation/mds_array_manipulation.py
@@ -0,0 +1,26 @@
+def search_array(arr, elem):
+    """
+    Search for elem in a numpy array.
+    Parameters
+    ----------
+    arr : numpy.array
+        The numpy array to search
+    elem : object to search for in the array
+    Returns
+    -------
+    int
+        Index of the first occurence of the element in the array, -1 if not found
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from mds_array_manipulation import search_array
+    >>> arr = np.array([20, 10, 20, 30, 50, 90, 60])
+    >>> search_array(arr, 50)
+    4
+    >>> search_array(arr, 100)
+    -1
+    >>> search_array(arr, 20)
+    0
+    """
+    # TODO: actual code
+    pass
