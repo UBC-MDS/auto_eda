6f41a74b4b271dc941ff70bf0ac3f441e3b63475
Yan Zeng <yzeng11@student.ubc.ca>
2024-01-18 10:45:32 -0800

    add more tests

--- tests/test_mds_array_manipulation.py
+++ tests/test_mds_array_manipulation.py
@@ -27,19 +27,27 @@ def test_input_not_numpy_array():
 
 # checks if a single-element array is returned as is
 def test_single_element_array_1(one_d_int_array_one_element):
-    assert np.array_equal(am.sort_array(one_d_int_array_one_element), one_d_int_array_one_element)
+    result = am.sort_array(one_d_int_array_one_element)
+    assert np.array_equal(result, one_d_int_array_one_element)
 
 def test_single_element_array_2(one_d_string_array_one_element):
-    assert np.array_equal(am.sort_array(one_d_string_array_one_element), one_d_string_array_one_element)
+    result = am.sort_array(one_d_string_array_one_element)
+    assert np.array_equal(result, one_d_string_array_one_element)
 
 # checks if an empty array is returned as is
 def test_empty_array(empty_array):
-    assert np.array_equal(am.sort_array(empty_array), empty_array)
+    result = am.sort_array(empty_array)
+    assert np.array_equal(result, empty_array)
 
 # checks if the function can sort arrays of integers and floats
 def test_sorts_numeric_types(one_d_int_array, one_d_float_array):
     assert np.array_equal(am.sort_array(one_d_int_array), np.array(sorted(one_d_int_array)))
     assert np.array_equal(am.sort_array(one_d_float_array), np.array(sorted(one_d_float_array)))
+
+# checks if the function can sort arrays of strings
+def test_sorts_numeric_types(one_d_string_array):
+    result = am.sort_array(one_d_string_array)
+    assert np.array_equal(result, np.array(sorted(one_d_string_array)))  
 
 # checks if a TypeError is raised for arrays with mixed data types
 def test_input_different_type():
@@ -48,12 +56,15 @@ def test_input_different_type():
 
 # checks if the function can sort arrays with negative numbers
 def test_negative_input(one_d_neg_array):
-    assert np.array_equal(am.sort_array(one_d_neg_array), np.array(sorted(one_d_neg_array)))
+    result = am.sort_array(one_d_neg_array)
+    assert np.array_equal(result, np.array(sorted(one_d_neg_array)))
 
 # checks if the function can sort a mixed-case string array in a case-insensitive manner
 def test_lowercase_uppercase_strings(one_d_string_array_mixed_case):
-    assert np.array_equal(am.sort_array(one_d_string_array_mixed_case), np.array(sorted(one_d_string_array_mixed_case, key=str.lower)))
+    result = am.sort_array(one_d_string_array_mixed_case)
+    assert np.array_equal(result, np.array(sorted(one_d_string_array_mixed_case, key=str.lower)))
 
 # checks if a sorted array remains unchanged after sorting again
 def test_already_sorted_array(one_d_int_array_sorted):
-    assert np.array_equal(am.sort_array(one_d_int_array_sorted), one_d_int_array_sorted)
+    result = am.sort_array(one_d_int_array_sorted)
+    assert np.array_equal(result, one_d_int_array_sorted)
