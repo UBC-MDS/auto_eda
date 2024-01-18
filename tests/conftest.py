6f40d8eca275154511b44c6010c3f77bebcd43c0
Kittipong Wongwipasamitkun <63245841+jokittipong@users.noreply.github.com>
2024-01-18 07:17:51 -0800

    Fixed minor things in conftest.py by adding [ ]
    Adding [ ]

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,13 +39,13 @@ def two_d_int_array():
 
 @pytest.fixture
 def two_d_float_array():
-    return np.array([1.0,5.1,3.14,9.62,18.88,24.01,0.0,1.5], \
-                    [10.0,1.01,0.01,25.67,-1.4444,-200.2,4.8,33.3])
+    return np.array([[1.0,5.1,3.14,9.62,18.88,24.01,0.0,1.5], \
+                    [10.0,1.01,0.01,25.67,-1.4444,-200.2,4.8,33.3]])
 
 @pytest.fixture
 def two_d_string_array():
-    return np.array(['dog', 'cat', 'banana', 'apple', 'frog'], \
-                    ['elephant', 'soup', 'aardvark', 'mouse', 'power'])
+    return np.array([['dog', 'cat', 'banana', 'apple', 'frog'], \
+                    ['elephant', 'soup', 'aardvark', 'mouse', 'power']])
 
 @pytest.fixture
 def three_d_int_array():
