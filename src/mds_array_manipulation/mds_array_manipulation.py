3ab1a0e2c5f5c365e902023a5301e9ca1614ea04
Yan Zeng <yzeng11@student.ubc.ca>
2024-01-18 13:02:23 -0800

    move sort_array to a separate file

--- src/mds_array_manipulation/mds_array_manipulation.py
+++ src/mds_array_manipulation/mds_array_manipulation.py
@@ -29,37 +29,6 @@ def search_array(arr, elem):
     pass
 
 
-def sort_array(arr):
-    """
-    Sort a numpy array in ascending or alphabetical order.
-
-    Parameters
-    ----------
-    arr : numpy.array
-        The numpy array to be sorted. The array can contain numerical 
-        or string values.
-
-    Returns
-    -------
-    numpy.array
-        A new numpy array sorted in ascending or alphabetical order.
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from mds_array_manipulation import mds_array_manipulation as am
-    >>> arr = np.array([20, 10, 40, 30, 50, 90, 60])
-    >>> sort_array(arr)
-    array([10, 20, 30, 40, 50, 60, 90])
-
-    >>> arr_str = np.array(["orange", "grape", "apple"])
-    >>> sort_array(arr_str)
-    array(['apple', 'grape', 'orange'])
-    """
-    # TODO: actual code
-    pass
-
-
 def count_nonzero_elements(arr):
     """
     Count the number of non zero elements in an array.
